b8d42eaf8924fb323d7d097e7025af2f8f68ffe5
sonng147202 <sonnguyen20140720@gmail.com>
2024-01-28 12:00:09 +0700

    update

---
+++ GlobeWork/Controllers/EmployerController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GlobeWork.Controllers
+{
+    public class EmployerController : Controller
+    {
+        // GET: Employer
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult Header()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult Sidebar()
+        {
+            return PartialView();
+        }
+    }
+}
---
+++ GlobeWork/Scripts/scripts2.js
@@ -0,0 +1,75 @@
+﻿function StudyAbroad() {
+    $(".study-abroad").slick({
+        dots: false,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        speed: 500,
+        arrows: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        prevArrow: '<button class="chevron-prev"><i class="fal fa-chevron-left"></i></button>',
+        nextArrow: '<button class="chevron-next"><i class="fal fa-chevron-right"></i></button>',
+    });
+
+    $(".scholarship").slick({
+        dots: false,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        speed: 800,
+        arrows: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        prevArrow: '<button class="chevron-prev"><i class="fal fa-chevron-left"></i></button>',
+        nextArrow: '<button class="chevron-next"><i class="fal fa-chevron-right"></i></button>',
+    });
+
+    $('.study-abroad-grid').slick({
+        rows: 5,
+        dots: false,
+        arrows: true,
+        infinite: true,
+        speed: 800,
+        slidesToShow: 3,
+        //fade: true,
+        //cssEase: 'linear',
+        slidesToScroll: 3,
+        prevArrow: '<button class="previous-slie"><i class="fal fa-chevron-circle-left"></i></button>',
+        nextArrow: '<button class="next-slie"><i class="fal fa-chevron-circle-right"></i></button>',
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 6,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: false
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3
+                }
+            }
+        ]
+    });
+}
+
+$(document).ready(function () {
+
+})
+
+function toggleSidebar() {
+    $(".sidebar").toggleClass("collapsed");
+}
